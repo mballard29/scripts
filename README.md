06b3810ae0dc33127bb6bd829a136f1f0ef3c76b
mballard29 <mmballard29@gmail.com>
2025-03-13 15:51:22 -0400

    txt to pdf

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ I use Pinterest to create moodboards, or just collect stuff I find on Pinterest 
 The commands I don't like to type out when creating a Github repo from an existing folder on my local machine. Skips me a couple steps forward to actually naming and setting permissions for the repo on Github using the CLI.
 
 ### Udemy Transcripts
-Made to allow me get Udemy transcrips in an annotatable format. Parses HTML to create output text files to be printed or copied/moved to iCloud folder for annotation using iPad. (Example files in folders to understand input/output functioning of script. I'm trying to respect IP, so they are redacted to only show what is necessary)
+Made to allow me get Udemy transcrips in an annotatable format. Parses HTML to create output pdf files to be printed or copied/moved to iCloud folder for annotation using iPad. (Example files in folders to understand input/output functioning of script. I'm trying to respect IP, so they are redacted to only show what is necessary)
 (*note: Udemy is not very developer friendly. Makes sense because the content hosted is paid/proprietary. The purpose of the script is to increase accessibility friendliness and studying flexibility. Specific issues mentioned in comments in script.)
 
 
--- UdemyTranscripts/scrape.py
+++ UdemyTranscripts/scrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import os
+from fpdf import FPDF
 
 print('''
 \n
@@ -22,14 +23,13 @@ Workflow (Mac):
 )
 
 # clean out old put folders before run
-# change path if necessary
-path = '<path to project>/ouput'
+path = '<project path>/output'
 old_out = os.listdir(path)
 for o in old_out:
     os.remove(f'output/{o}')
 
 # change if necessary
-path = '<path to project>/sources'
+path = '<project path>/sources'
 sources = os.listdir(path)
 
 # "scraping" date
@@ -52,8 +52,20 @@ for s in sources:
   title = title[(title.find(',')+1):(title.find('(')-1)]
   fn = 'output/' + title + '.txt'
 
-  with open(fn, "w") as of:
-      of.write(transcript)
+  # output to pdf
+  pdf = FPDF(format='letter', unit='in')
+  pdf.add_page()
+  pdf.set_font("helvetica", size = 12)
+  pdf.set_margins(1, 1)
+  effective_page_width = pdf.w - 2*pdf.l_margin
+  pdf.multi_cell(
+      w=effective_page_width, 
+      h=0.22, 
+      text=transcript,
+      align='J'
+      )
+  fn = 'output/' + title + '.pdf'
+  pdf.output(fn)
 
   # clean out sources folder for next run
   os.remove(f'source/{s}')
@@ -67,12 +79,4 @@ Issues:
 - The page is dynamically loaded and looking at the Inspect window, the HTML will only show explicitly visible content
   - e.g. Video titles (inner text) not visible in inspect window if the page section is not large enough to toggle visibility
 - No FETCH/XHR requests are discernably for transcript data
-
-Future improvement:
--[ ] refactor as python script that takes commandline arguments to allow drag source and drop onto executable to run.
--[ ] refactor to open browser, get user to manually log in, and use tool like selenium to get html from videos in a section
-    Loop:
-    - click open transcript button
-    - directly scrape html
-    - click next-video button
 '''
