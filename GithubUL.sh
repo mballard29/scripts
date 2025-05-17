991345eaf973dd2e4ff7777bbb5511f1b08e8525
Mason Ballard <60182637+mballard29@users.noreply.github.com>
2025-05-17 10:46:48 -0400

    Update GithubUL.sh

--- GithubUL.sh
+++ GithubUL.sh
@@ -4,3 +4,6 @@ git init && git symbolic-ref HEAD refs/heads/main
 git add .
 git commit -m "First commit"
 gh repo create
+
+# for folders or storage locations that don't track ownership (flashdrive), may need:
+# git config --global --add safe.directory '<folder path>'
